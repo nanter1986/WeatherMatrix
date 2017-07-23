3e7c239e2d818bf72624d235107fe0f8600f9c16
nanter1986 <nanter1986@gmail.com>
2017-07-23 10:45:11 +0300

    got position,now weather

--- app/src/main/java/com/weathermatrix/nanter1986/weathermatrix/MainActivity.java
+++ app/src/main/java/com/weathermatrix/nanter1986/weathermatrix/MainActivity.java
@@ -1,23 +1,21 @@
 package com.weathermatrix.nanter1986.weathermatrix;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.media.Image;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -28,8 +26,10 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
     Document doc;
     Document doc2;
     Elements img;
@@ -38,23 +38,110 @@ public class MainActivity extends Activity {
     String selectedURL;
     ArrayList<String> resultUrls = new ArrayList<String>();
     ImageView displayImage;
+    TextView displayText;
+    Float myLatitude;
+    Float myLongitude;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Geocoder geocoder;
+    List<Address> addresses;
+    String city;
+    String state;
+    String country;
+    String postalCode;
+    String knownName;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayImage=findViewById(R.id.displayImage);
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        displayImage = (ImageView) findViewById(R.id.displayImage);
+        displayText = (TextView) findViewById(R.id.displayText);
+        getLocation();
+        getLocationInfo();
 
         new GetDocument().execute();
 
 
     }
 
+    private void getLocationInfo() {
+        myLatitude=sharedPreferences.getFloat("lat",0);
+        myLongitude=sharedPreferences.getFloat("lon",0);
+        Double dLat=(double)myLatitude;
+        Double dLLon=(double)myLongitude;
+        TheLogger.myLog("1", dLat +" "+ dLLon);
+        TheLogger.myLog("1","in info");
+        if(myLatitude ==null || myLongitude ==null){
+            TheLogger.myLog("1","in null");
+            displayText.setText("something is null\n"+ myLongitude +"\n"+ myLatitude);
+        }else {
+            TheLogger.myLog("1","in else");
+
+            try {
+                geocoder = new Geocoder(this, Locale.getDefault());
+                TheLogger.myLog("1","in try1");
+                addresses = geocoder.getFromLocation(myLatitude,myLongitude, 1);
+                TheLogger.myLog("1","in try2");
+
+                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                city = addresses.get(0).getLocality();
+                state = addresses.get(0).getAdminArea();
+                country = addresses.get(0).getCountryName();
+                postalCode = addresses.get(0).getPostalCode();
+                knownName = addresses.get(0).getFeatureName();
+                displayText.setText(city);
+                TheLogger.myLog("1","in try2");
+                TheLogger.myLog("1","city:"+city);
+                TheLogger.myLog("1","adress:"+address);
+                TheLogger.myLog("1","state:"+state);
+                TheLogger.myLog("1","country:"+country);
+                TheLogger.myLog("1","postal:"+postalCode);
+                TheLogger.myLog("1","known name:"+knownName);
+            } catch (IOException e) {
+                TheLogger.myLog("1","in catch");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getLocation() {
+
+        displayText.setText("Getting location...");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                myLatitude=(float)location.getLatitude();
+                                myLongitude=(float)location.getLongitude();
+                                editor.putFloat("lat", myLatitude);
+                                editor.putFloat("lon", myLongitude);
+                                editor.commit();
+                                displayText.setText("Longitude:"+myLongitude+"\nLatitude:"+myLatitude);
+
+                            }
+                        }
+                    });
+        }catch (SecurityException se){
+            se.printStackTrace();
+        }
+    }
+
+
     public class GetDocument extends AsyncTask<Void,Void,Void>{
 
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                doc= Jsoup.connect("https://www.google.gr/search?q=chania+rainy&client=ubuntu&hs=QMm&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiCqbiZvpnVAhWsI8AKHVSpD4kQ_AUICigB&biw=1301&bih=323").get();
+                doc= Jsoup.connect("https://www.google.gr/search?q="+city+"+rainy&client=ubuntu&hs=QMm&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiCqbiZvpnVAhWsI8AKHVSpD4kQ_AUICigB&biw=1301&bih=323").get();
                 TheLogger.myLog("1","Grabbed document");
                 img = doc.select("img[data-src]");
                 TheLogger.myLog("2","Array created of size"+" "+img.size());
