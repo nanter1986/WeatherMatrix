2eca48e411bf59939f86c7f0fca58abc0e6ab11b
nanter1986 <nanter1986@gmail.com>
2017-07-27 07:06:04 +0300

    flip working,need togeneralze weather grabbing

--- app/src/main/java/com/weathermatrix/nanter1986/weathermatrix/MainActivity.java
+++ app/src/main/java/com/weathermatrix/nanter1986/weathermatrix/MainActivity.java
@@ -11,9 +11,11 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -42,8 +44,23 @@ public class MainActivity extends Activity {
     Bitmap theBitmap;
     String selectedURL;
     ArrayList<String> resultUrls = new ArrayList<String>();
-    ImageView displayImage;
-    TextView displayText;
+
+    ImageView zeroDisplayImage;
+    ImageView oneDisplayImage;
+    ImageView twoDisplayImage;
+    ImageView threeDisplayImage;
+    ImageView fourDisplayImage;
+    ImageView fiveDisplayImage;
+    ImageView sixDisplayImage;
+    TextView zeroDisplayText;
+    TextView oneDisplayText;
+    TextView twoDisplayText;
+    TextView threeDisplayText;
+    TextView fourDisplayText;
+    TextView fiveDisplayText;
+    TextView sixDisplayText;
+
+
     Float myLatitude;
     Float myLongitude;
     SharedPreferences sharedPreferences;
@@ -60,8 +77,26 @@ public class MainActivity extends Activity {
 
     Context context;
     Handler handler;
+
+    private ViewFlipper mViewFlipper;
+    private float initialX;
+
     private FusedLocationProviderClient mFusedLocationClient;
 
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,9 +104,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         sharedPreferences = getPreferences(MODE_PRIVATE);
         editor = sharedPreferences.edit();
-        displayImage = (ImageView) findViewById(R.id.zeroDisplayImage);
-        displayText = (TextView) findViewById(R.id.zeroDisplayText);
-
+        mViewFlipper = this.findViewById(R.id.view_flipper);
+        referenceImageviewsAndTextviews();
         //http://www.journaldev.com/10429/android-viewflipper-example-tutorial
 
         handler = new Handler();
@@ -82,6 +116,69 @@ public class MainActivity extends Activity {
         //getWeather();
 
         new GetDocument().execute();
+
+
+    }
+
+    private void referenceImageviewsAndTextviews() {
+        zeroDisplayImage = findViewById(R.id.zeroDisplayImage);
+        zeroDisplayText = findViewById(R.id.zeroDisplayText);
+
+        oneDisplayImage = findViewById(R.id.oneDisplayImage);
+        oneDisplayText = findViewById(R.id.oneDisplayText);
+
+        twoDisplayImage = findViewById(R.id.twoDisplayImage);
+        twoDisplayText = findViewById(R.id.twoDisplayText);
+
+        threeDisplayImage = findViewById(R.id.threeDisplayImage);
+        threeDisplayText = findViewById(R.id.threeDisplayText);
+
+        fourDisplayImage = findViewById(R.id.fourDisplayImage);
+        fourDisplayText = findViewById(R.id.fourDisplayText);
+
+        fiveDisplayImage = findViewById(R.id.fiveDisplayImage);
+        fiveDisplayText = findViewById(R.id.fiveDisplayText);
+
+        sixDisplayImage = findViewById(R.id.sixDisplayImage);
+        sixDisplayText = findViewById(R.id.sixDisplayText);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initialX = touchevent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                float finalX = touchevent.getX();
+                if (initialX > finalX) {
+                    if (mViewFlipper.getDisplayedChild() == 6){
+                        TheLogger.myLog("limit scroll","you reached the end");
+                        break;
+                    }
+
+
+                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(context, R.anim.in_from_left));
+                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(context, R.anim.out_from_left));
+                    TheLogger.myLog("next","showing next card");
+                    mViewFlipper.showNext();
+                } else {
+                    if (mViewFlipper.getDisplayedChild() == 0){
+                        TheLogger.myLog("limit scroll","you reached the end");
+                        break;
+                    }
+
+
+                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(context, R.anim.in_from_right));
+                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(context, R.anim.out_from_right));
+                    TheLogger.myLog("previous","showing previous card");
+                    mViewFlipper.showPrevious();
+                }
+                break;
+        }
+
+
+        return false;
 
 
     }
@@ -100,6 +197,90 @@ public class MainActivity extends Activity {
         }
 
 
+    }
+
+    public void displayWeather(JSONObject json) {
+        try {
+            JSONObject full = json.getJSONArray("list").getJSONObject(0).getJSONObject("main");
+            TheLogger.myLog("weather", "cool1");
+            String temperature = full.getString("temp");
+            zeroDisplayText.setText(String.format("%.2f", Double.parseDouble(temperature)) + " ℃");
+            TheLogger.myLog("weather", "cool2");
+            //weatherCondition = json.getJSONArray("weather").getJSONObject(0).getString("main");
+            TheLogger.myLog("weather", "cool3");
+            TheLogger.myLog("weather", weatherCondition.toString());
+        } catch (JSONException e) {
+            TheLogger.myLog("weather", "something wrong");
+            e.printStackTrace();
+        }
+    }
+
+    private void getLocationInfo() {
+        myLatitude = sharedPreferences.getFloat("lat", 0);
+        myLongitude = sharedPreferences.getFloat("lon", 0);
+        Double dLat = (double) myLatitude;
+        Double dLLon = (double) myLongitude;
+        TheLogger.myLog("1", dLat + " " + dLLon);
+        TheLogger.myLog("1", "in info");
+        if (myLatitude == null || myLongitude == null) {
+            TheLogger.myLog("1", "in null");
+            zeroDisplayText.setText("something is null\n" + myLongitude + "\n" + myLatitude);
+        } else {
+            TheLogger.myLog("1", "in else");
+
+            try {
+                geocoder = new Geocoder(this, Locale.getDefault());
+                TheLogger.myLog("1", "in try1");
+                addresses = geocoder.getFromLocation(myLatitude, myLongitude, 1);
+                TheLogger.myLog("1", "in try2");
+
+                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                city = addresses.get(0).getLocality();
+                state = addresses.get(0).getSubAdminArea();
+                country = addresses.get(0).getCountryName();
+                postalCode = addresses.get(0).getPostalCode();
+                knownName = addresses.get(0).getFeatureName();
+                zeroDisplayText.setText(city);
+                TheLogger.myLog("1", "in try2");
+                TheLogger.myLog("1", "city:" + city);
+                TheLogger.myLog("1", "adress:" + address);
+                TheLogger.myLog("1", "state:" + state);
+                TheLogger.myLog("1", "country:" + country);
+                TheLogger.myLog("1", "postal:" + postalCode);
+                TheLogger.myLog("1", "known name:" + knownName);
+                TheLogger.myLog("1", "thourough:" + addresses.get(0).getThoroughfare());
+                TheLogger.myLog("1", "Sthourough:" + addresses.get(0).getSubThoroughfare());
+            } catch (IOException e) {
+                TheLogger.myLog("1", "in catch");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getLocation() {
+
+        zeroDisplayText.setText("Getting location...");
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                myLatitude = (float) location.getLatitude();
+                                myLongitude = (float) location.getLongitude();
+                                editor.putFloat("lat", myLatitude);
+                                editor.putFloat("lon", myLongitude);
+                                editor.commit();
+                                zeroDisplayText.setText("Longitude:" + myLongitude + "\nLatitude:" + myLatitude);
+
+                            }
+                        }
+                    });
+        } catch (SecurityException se) {
+            se.printStackTrace();
+        }
     }
 
     public class GetWeatherAsync extends AsyncTask<Void, Void, Void> {
@@ -124,91 +305,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void displayWeather(JSONObject json) {
-        try {
-            JSONObject full = json.getJSONArray("list").getJSONObject(0).getJSONObject("main");
-            TheLogger.myLog("weather", "cool1");
-            String temperature=full.getString("temp");
-            displayText.setText(String.format("%.2f", Double.parseDouble(temperature)) + " ℃");
-            TheLogger.myLog("weather", "cool2");
-            //weatherCondition = json.getJSONArray("weather").getJSONObject(0).getString("main");
-            TheLogger.myLog("weather", "cool3");
-            TheLogger.myLog("weather", weatherCondition.toString());
-        } catch (JSONException e) {
-            TheLogger.myLog("weather", "something wrong");
-            e.printStackTrace();
-        }
-    }
-
-    private void getLocationInfo() {
-        myLatitude = sharedPreferences.getFloat("lat", 0);
-        myLongitude = sharedPreferences.getFloat("lon", 0);
-        Double dLat = (double) myLatitude;
-        Double dLLon = (double) myLongitude;
-        TheLogger.myLog("1", dLat + " " + dLLon);
-        TheLogger.myLog("1", "in info");
-        if (myLatitude == null || myLongitude == null) {
-            TheLogger.myLog("1", "in null");
-            displayText.setText("something is null\n" + myLongitude + "\n" + myLatitude);
-        } else {
-            TheLogger.myLog("1", "in else");
-
-            try {
-                geocoder = new Geocoder(this, Locale.getDefault());
-                TheLogger.myLog("1", "in try1");
-                addresses = geocoder.getFromLocation(myLatitude, myLongitude, 1);
-                TheLogger.myLog("1", "in try2");
-
-                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-                city = addresses.get(0).getLocality();
-                state = addresses.get(0).getSubAdminArea();
-                country = addresses.get(0).getCountryName();
-                postalCode = addresses.get(0).getPostalCode();
-                knownName = addresses.get(0).getFeatureName();
-                displayText.setText(city);
-                TheLogger.myLog("1", "in try2");
-                TheLogger.myLog("1", "city:" + city);
-                TheLogger.myLog("1", "adress:" + address);
-                TheLogger.myLog("1", "state:" + state);
-                TheLogger.myLog("1", "country:" + country);
-                TheLogger.myLog("1", "postal:" + postalCode);
-                TheLogger.myLog("1", "known name:" + knownName);
-                TheLogger.myLog("1", "thourough:" + addresses.get(0).getThoroughfare());
-                TheLogger.myLog("1", "Sthourough:" + addresses.get(0).getSubThoroughfare());
-            } catch (IOException e) {
-                TheLogger.myLog("1", "in catch");
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void getLocation() {
-
-        displayText.setText("Getting location...");
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        try {
-            mFusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // Got last known location. In some rare situations this can be null.
-                            if (location != null) {
-                                myLatitude = (float) location.getLatitude();
-                                myLongitude = (float) location.getLongitude();
-                                editor.putFloat("lat", myLatitude);
-                                editor.putFloat("lon", myLongitude);
-                                editor.commit();
-                                displayText.setText("Longitude:" + myLongitude + "\nLatitude:" + myLatitude);
-
-                            }
-                        }
-                    });
-        } catch (SecurityException se) {
-            se.printStackTrace();
-        }
-    }
-
-
     public class GetDocument extends AsyncTask<Void, Void, Void> {
 
         @Override
@@ -221,8 +317,8 @@ public class MainActivity extends Activity {
                     //weatherCondition = json.getJSONArray("weather").getJSONObject(0).getString("main");
                     weatherCondition = json.getJSONArray("list").getJSONObject(0).getJSONArray("weather").getJSONObject(0).getString("main");
                     editor.putString("json1", json.toString());
-                    TheLogger.myLog("10", "JSON is cool: "+json.toString());
-                    TheLogger.myLog("10", "small JSON is cool: "+weatherCondition);
+                    TheLogger.myLog("10", "JSON is cool: " + json.toString());
+                    TheLogger.myLog("10", "small JSON is cool: " + weatherCondition);
                     editor.commit();
                     //displayWeather(json);
                 }
@@ -265,30 +361,14 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            displayImage.setImageBitmap(theBitmap);
+            zeroDisplayImage.setImageBitmap(theBitmap);
             try {
-                JSONObject json=new JSONObject(sharedPreferences.getString("json1",""));
+                JSONObject json = new JSONObject(sharedPreferences.getString("json1", ""));
                 displayWeather(json);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-        }
-    }
-
-
-    public static Bitmap getBitmapFromURL(String src) {
-        try {
-            URL url = new URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            // Log exception
-            return null;
         }
     }
 
